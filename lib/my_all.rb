bfe7a20a381c01409bc54150deee864bdeb5f9e6
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:33:07 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  if collection != []
-    while i < collection.length
-      block_return_values << yield(collection[1])
-      i = i + 1 
-    end
+  while i < collection.length
+    block_return_values << yield(collection[1])
+    i = i + 1 
   end
 end
