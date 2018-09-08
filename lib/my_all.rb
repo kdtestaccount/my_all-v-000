497b3a1628daa95383d18e55d54f75db35df1ec5
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:34:27 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,15 +2,17 @@ require 'pry'
 
 def my_all?(collection)
   if collection != []
-  i = 0
-  while i < collection.length
-    block_return_values << yield(collection[1])
-    i = i + 1 
-  end
+    i = 0
+    while i < collection.length
+      block_return_values << yield(collection[1])
+      i = i + 1 
+    end
   
-  if block_return_values.include?(false)
-    false
+    if block_return_values.include?(false)
+      false
+    else
+      true 
+    end
   else
-    true 
-  end
+    "No data in collection"
 end
