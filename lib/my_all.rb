6b9c9ecdf2cf30ea1b2eaab2be1f59fec4d88879
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:32:54 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   if collection != []
     while i < collection.length
-      new_collection << yield(collection[1])
+      block_return_values << yield(collection[1])
       i = i + 1 
     end
   end
